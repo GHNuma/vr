1a3dece16ca79deed0ace6c83a77c9534e52bebf
GHNuma <mr.ghnuma@gmail.com>
2024-10-13 21:44:32 +0500

    feat: some redesign

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        primary:'#ade746'
+      },
+      backgroundImage: {
+        'room1': "url('/src/assets/vr_room3.jpeg')",
+        'room2': "url('/src/assets/vr_room1.jpeg')",
+        'room3': "url('/src/assets/vr_room2.jpeg')",
+        'room4': "url('/src/assets/vr_room4.jpeg')",
+      }
+
+    },
+  },
+  plugins: [],
+}
